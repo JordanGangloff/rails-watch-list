7dc0bb0be922e5f8d50deae967eb6c517a6f7453
Jordan Gangloff <jordangangloff@icloud.com>
2022-08-18 17:06:45 +0200

    bof bof

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: :destroy
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,20 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get '/lists', to: 'Lists#index'
+  # get '/lists/:id', to: 'Lists#show'
+
+  # get '/lists/new', to: 'Lists#new'
+  # post '/lists', to: 'Lists#create'
+
+  # get '/bookmarks/new', to: 'Bookmarks#new'
+  # post '/bookmarks', to: 'Bookmarks#create'
+
+  # delete '/bookmarks/:id', to: 'Bookmarks#destroy'
+
+  resources :lists do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
 end
